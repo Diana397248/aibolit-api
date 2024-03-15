e3e999a58e7d258b93e0d2e9c200beb081e1eb31
Diana <du397248@gmail.com>
2024-03-15 22:58:09 +0400

    init

---
+++ database/migrations/2024_03_15_181117_create_appointments_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('appointments', function (Blueprint $table) {
+            $table->id();
+            $table->dateTime('app_date');
+            $table->text('reason');
+            $table->enum('status',
+                [
+                    'approved',
+                    'expectation',
+                    'awaiting_payment',
+                    'completed',
+                    'denied'
+                ]);
+            $table->timestamps();
+
+            $table->unsignedBigInteger('client_id')->index();
+            $table->foreign('client_id')->references('id')->on('users');
+
+            //todo pet_id vet_id
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('appointments');
+    }
+};
