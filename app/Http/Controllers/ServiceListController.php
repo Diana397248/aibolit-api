c07e768e767ddd1b6fbf63f4debc0598fffbae1c
Diana <du397248@gmail.com>
2024-03-21 22:30:41 +0400

    Vet_services service_lists  service_Categor

---
+++ app/Http/Controllers/ServiceListController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ServiceList;
+use App\Http\Requests\StoreServiceListRequest;
+use App\Http\Requests\UpdateServiceListRequest;
+
+class ServiceListController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreServiceListRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ServiceList $serviceList)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ServiceList $serviceList)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateServiceListRequest $request, ServiceList $serviceList)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ServiceList $serviceList)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/VetServiceController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\VetService;
+use App\Http\Requests\StoreVetServiceRequest;
+use App\Http\Requests\UpdateVetServiceRequest;
+
+class VetServiceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreVetServiceRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(VetService $vetService)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(VetService $vetService)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateVetServiceRequest $request, VetService $vetService)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(VetService $vetService)
+    {
+        //
+    }
+}
